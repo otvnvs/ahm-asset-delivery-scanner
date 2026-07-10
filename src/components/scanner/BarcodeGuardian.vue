9a60b7f104faeaec51b70316034b0321ac92a5ef
skullquake <ockert8080@gmail.com>
2026-07-10 16:25:18 +0200

    scanner
    Signed-off-by: skullquake <ockert8080@gmail.com>

--- src/components/scanner/BarcodeGuardian.vue
+++ src/components/scanner/BarcodeGuardian.vue
@@ -20,7 +20,6 @@
 <script setup>
 import { onMounted, onUnmounted, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { initWindowOverrides } from './components/dialog/useDialog.js';
 import { isWebcamScannerOpen } from '../../util/barcodeScanner.js';
 import { store } from './util/store.js';
 
